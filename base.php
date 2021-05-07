69941695d6ecfc2f1a6dd49f2405bfeb11c292aa
altairObando <pxnfilo@gmail.com>
2021-05-07 17:03:02 -0600

    CRUD financieraController

--- base.php
+++ base.php
@@ -7,7 +7,7 @@
 Plugin Name: Plugin Financiera CR
 Plugin URI : https://github.com/altairObando/Plugin-Financiera.git
 Description: Simple plugin que crea una tabla de base de datos en wordpress
-Version 0.1
+Version: 1
 Author : Noel Antonio Obando Espinoza
 Author URI: https://github.com/altairObando/
 */
@@ -15,9 +15,11 @@ Author URI: https://github.com/altairObando/
 defined('ABSPATH') or die('Algo salio mal :(');
 
 include(plugin_dir_path(__FILE__).'includes/register.php');
-
-
+include(plugin_dir_path(__FILE__).'includes/financieraController.php');
 register_activation_hook(__FILE__, 'register_tables');
 register_deactivation_hook(__FILE__, 'delete_tables');
+
+
+add_action('rest_api_init', 'financiera');
 
 ?>
---
+++ includes/financieraController.php
@@ -0,0 +1,71 @@
+<?php
+    function financiera(){
+        register_rest_route('financiera/v1','financieras/', [
+            'methods' => 'GET',
+            'callback' => 'get',
+            'args' => [ 'id' => ['required' => false, 'type' => 'number']]
+        ]);
+        register_rest_route('financiera/v1', 'financieras/',[
+            'methods' => 'POST',
+            'callback' => 'post',
+            'args' => [
+                'nombre' => ['required' => true]
+            ]
+        ]);
+        register_rest_route('financiera/v1', 'financieras/(?P<id>\d+)',[
+            'methods' => 'PUT',
+            'callback' => 'put',
+            'args' => [
+                'id' => ['required' => true, 'type' => 'number'],
+                'nombre' => ['required' => false],
+                'rangoDeudorId' => ['required' => false]
+
+            ]
+        ]);
+    };
+    
+    function get($request){
+        global $wpdb;
+		$tablaFinanciera = $wpdb->prefix."financiera";
+        $id = $request->get_param('id');
+        
+        $query = "SELECT * FROM $tablaFinanciera ";
+
+        if($id !== NULL && $id > 0 )
+            $query = $query." WHERE id = $id";
+
+        $response = $wpdb->get_results($query, ARRAY_A);
+        return rest_ensure_response($response);
+    }
+    function post($request){
+        global $wpdb;
+        $tablaFinanciera = $wpdb->prefix."financiera";
+        
+        $nombre = $request->get_param('nombre');
+        $rangoDeudorId = $request['rangoDeudorId'];
+        $newFinanciera = array(
+            'nombre' => $nombre,
+            'rangoDeudorId' => $rangoDeudorId
+        );
+
+        $wpdb->insert($tablaFinanciera, $newFinanciera);
+
+        return rest_ensure_response($newFinanciera);
+    }
+    function put($request){
+		global $wpdb;
+        $tablaFinanciera = $wpdb->prefix."financiera";
+		$id = $request->get_param('id');
+		$nombre = $request->get_param('nombre');
+        $rangoDeudorId = $request->get_param('rangoDeudorId');
+        $updateFinanciera = array(
+            'nombre' => $nombre,
+            'rangoDeudorId' => $rangoDeudorId
+        );
+		$wpdb->update($tablaFinanciera,$updateFinanciera, array('id' => $id));
+		return rest_ensure_response(array('ok' => true,'mensaje' => 'Financiera Actualizada.'));
+    }
+    function delete($request){
+
+    }
+?>
