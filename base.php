1f7f18a46e908230a612da514a24a6715cd5feba
altairObando <pxnfilo@gmail.com>
2021-05-26 07:41:17 -0600

    enable cors

--- base.php
+++ base.php
@@ -17,6 +17,7 @@ defined('ABSPATH') or die('Algo salio mal :(');
 include(plugin_dir_path(__FILE__).'includes/register.php');
 include(plugin_dir_path(__FILE__).'includes/financieraController.php');
 include(plugin_dir_path(__FILE__).'includes/calculoForm.php');
+include(plugin_dir_path(__FILE__).'includes/admin.php');
 register_activation_hook(__FILE__, 'register_tables');
 register_deactivation_hook(__FILE__, 'delete_tables');
 
@@ -24,5 +25,23 @@ register_deactivation_hook(__FILE__, 'delete_tables');
 add_action('rest_api_init', 'financiera');
 add_action('wp_head', 'wpb_hook_javascript');    
 add_shortcode( 'codesign_calculadora_form', 'cf_shortcode' );
+
+
+function initCors( $value ) {
+    $origin_url = '*';
+
+  
+    header( 'Access-Control-Allow-Origin: ' . $origin_url );
+    header( 'Access-Control-Allow-Methods: GET' );
+    header( 'Access-Control-Allow-Credentials: true' );
+    return $value;
+  }
+
+add_action( 'rest_api_init', function() {
+
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+
+	add_filter( 'rest_pre_serve_request', initCors);
+}, 15 );
 
 ?>
